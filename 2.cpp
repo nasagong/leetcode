8e0b3c753f92a350d08d107804cf9908042e3f53
nasagong <nasagong@naver.com>
2024-12-29 21:23:42 +0900

    2

---
+++ 2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* current = head;
+        int carry = 0;
+
+        while(l1 != nullptr || l2 != nullptr || carry){
+            int sum = carry;
+
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum/10;
+            current->next = new ListNode(sum%10);
+            current = current->next;
+        }
+
+        ListNode* result = head->next;
+        delete head;
+        return result;
+    }
+};
