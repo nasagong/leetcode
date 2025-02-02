98a0ebb9461f87a56e046ae93b45b18dadfe4c2c
nasagong <nasagong@naver.com>
2025-02-03 00:00:32 +0900

    139(2)

--- 139.cpp
+++ 139.cpp
@@ -17,3 +17,28 @@ public:
         return dp[s.size()];
     }
 };
+
+
+// editorial
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> arr(s.size(), false);  
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (i != 0 && !arr[i - 1]) 
+                continue;
+
+            for (string str : wordDict) {
+                if (str.size() + i <= s.size() && s.substr(i, str.size()) == str) {
+                    arr[str.size() + i - 1] = true;  
+
+                    if (arr[arr.size() - 1])  
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
