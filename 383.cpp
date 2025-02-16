adcf423e3e8c5eb601313f55f8a91e85102b77c1
nasagong <nasagong@naver.com>
2025-02-16 23:17:48 +0900

    383

---
+++ 383.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        bool canConstruct(string ransomNote, string magazine) {
+            unordered_map<char,int> magazineMap;
+            for(char c : magazine) magazineMap[c]++;
+    
+            for(char c : ransomNote) {
+                if(--magazineMap[c] < 0) return false;
+            }
+            return true;
+        }
+    };
